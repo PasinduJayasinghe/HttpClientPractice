49d3814b31da7e1dd69b0da55ec41c20ae7ffae2
PasinduJayasinghe <thisarajayasinghe07@gmail.com>
2024-12-03 17:27:56 +0530

    Delete

---
+++ HttpPracticeDelete/HttpPracticeDelete/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+		try
+		{
+            HttpClient client = new HttpClient();
+
+            String url = "https://jsonplaceholder.typicode.com/todos/2";
+            HttpResponseMessage response = await client.DeleteAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Success");
+            }
+            else {
+                Console.WriteLine($"Failed: {response.StatusCode}");
+                }
+
+        }
+		catch (HttpRequestException e)
+		{
+
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+}
