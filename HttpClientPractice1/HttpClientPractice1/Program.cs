b827866a5602a54e88011aa037e11012f2b3dcd0
PasinduJayasinghe <thisarajayasinghe07@gmail.com>
2024-12-03 13:27:37 +0530

    HttpClient GetAsync Practice

---
+++ HttpClientPractice1/HttpClientPractice1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Creating a httpclient object with "using" keyword ensuring proper resource management
+       using HttpClient client = new HttpClient();
+
+		try
+		{
+            string response = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+            Console.WriteLine(response);
+        }
+		catch (HttpRequestException e)
+		{
+            Console.WriteLine($"Request Error :{e.Message}");
+            
+		}
+    }
+}
